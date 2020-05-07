64d13deddb5b6ad822908ba0339bd2bbfe1015c8
Spencer Howell <spencerhowell98@gmail.com>
2020-05-06 20:38:02 -0500

    Add embedded YouTube video to the About page

--- lib/about.dart
+++ lib/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'strings.dart';
 
 class AboutScreen extends StatelessWidget {
@@ -6,27 +7,46 @@ class AboutScreen extends StatelessWidget {
       const TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold);
   final _sectionContent = const TextStyle(fontSize: 18.0);
 
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: 'WLrkT4F7tEc',
+    flags: YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.aboutTitle),
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(Strings.aboutHeadline, style: _sectionHeader),
-              Spacer(flex: 1),
-              Text(Strings.aboutPageParagraphOne, style: _sectionContent),
-              Spacer(flex: 1),
-              Text(Strings.aboutPageParagraphTwo, style: _sectionContent),
-              Spacer(flex: 1),
-              Text(Strings.aboutPageParagraphThree, style: _sectionContent),
-              Spacer(flex: 20),
-            ]),
-      ),
+      body: ListView(padding: EdgeInsets.all(16.0), children: <Widget>[
+        Column(children: <Widget>[
+          Container(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child:
+                  Text(Strings.aboutPageParagraphOne, style: _sectionContent)),
+          Container(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child:
+                  Text(Strings.aboutPageParagraphTwo, style: _sectionContent)),
+          Container(padding: EdgeInsets.symmetric(vertical: 8.0), child: Text(Strings.aboutPageParagraphThree, style: _sectionContent)),
+          YoutubePlayer(
+            controller: _controller,
+            showVideoProgressIndicator: true,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: RaisedButton(
+              onPressed: () {},
+              color: Theme.of(context).primaryColor,
+              child: Text(Strings.aboutPageDownloadLink.toUpperCase(),
+                  style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ]),
+      ]),
     );
   }
 }
--- lib/strings.dart
+++ lib/strings.dart
@@ -1,8 +1,10 @@
 class Strings {
   static String appTitle = "CARD Workout";
-  static String aboutTitle = "About";
-  static String aboutHeadline = "About CARD Workout";
+
+  // AboutScreen strings
+  static String aboutTitle = "About CARD Workout";
   static String aboutPageParagraphOne = "The CARD workout is a system for at-home exercise developed by bodybuilder and Youtuber Jeff Nippard.";
   static String aboutPageParagraphTwo = "The CARD workout is based on the principles of randomness to keep workouts motivating and fun.";
-  static String aboutPageParagraphThree = "To learn about the system, please watch the below video and download the program from Jeff's webiste.";
+  static String aboutPageParagraphThree = "To learn about the system, watch the below video and download the program from Jeff's website.";
+  static String aboutPageDownloadLink = "Download the program";
 }
