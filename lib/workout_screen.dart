93d463404b1642e3f9dde8dbc6f0a34cc970e46c
Spencer Howell <spencerhowell98@gmail.com>
2020-07-12 23:47:16 -0500

    Fix timer text formatting

--- lib/workout_screen.dart
+++ lib/workout_screen.dart
@@ -148,7 +148,9 @@ class _WorkoutScreenState extends State<WorkoutScreen>
       var duration = Duration(
           minutes: (_restCountdown / Duration.secondsPerMinute).floor(),
           seconds: (_restCountdown % Duration.secondsPerMinute));
-      return '${duration.toString()}';
+      var twoDigitMinutes = duration.inMinutes.remainder(60).toString().padLeft(2, "0");
+      var twoDigitSeconds = duration.inSeconds.remainder(60).toString().padLeft(2, "0");
+      return '$twoDigitMinutes:$twoDigitSeconds';
     } else {
       return '${_currentSet.goalReps} ' + Strings.exerciseReps;
     }
